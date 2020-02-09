c6d3ed49a9cc603e550ac1408fea949e3ca9fffb
Shohnurjon Zokirov <csws070799@gmail.com>
2020-02-10 00:51:12 +0500

    Initial commit

---
+++ app/src/main/java/uz/ideal/dictionary/MainActivity.kt
@@ -0,0 +1,12 @@
+package uz.ideal.dictionary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
