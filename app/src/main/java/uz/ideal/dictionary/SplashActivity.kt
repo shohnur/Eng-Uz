59c35193dff0dce8f6b69748939462ca8bc9e3f4
Dilshod Nurullayev <dilshod1845@gmail.com>
2020-02-10 15:58:04 +0500

    splash activity

---
+++ app/src/main/java/uz/ideal/dictionary/SplashActivity.kt
@@ -0,0 +1,24 @@
+package uz.ideal.dictionary
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT = 5000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        var handler = Handler().postDelayed(object : Runnable {
+            override fun run() {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            }
+
+        }, SPLASH_TIME_OUT.toLong())
+    }
+}
